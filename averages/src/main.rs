8aac60f6b3c270fedeedd01acf767ad7e939c62f
ashaunthomas <ashaunthomas@siu.edu>
2021-12-13 07:01:24 -0800

    vector input

--- averages/src/main.rs
+++ averages/src/main.rs
@@ -3,14 +3,11 @@
  * user inputs and then select the type of average they
  * want to output (mean, mediun, mode)
  */
+
+use std::io;
+
+
 fn main() {
-    print_title();
-    print_instructions();
-    
-}
-
-
-fn print_title() {
     println!(" 
      █████╗ ██╗   ██╗███████╗██████╗  █████╗  ██████╗ ███████╗██████╗          
     ██╔══██╗██║   ██║██╔════╝██╔══██╗██╔══██╗██╔════╝ ██╔════╝██╔══██╗         
@@ -21,9 +18,25 @@ fn print_title() {
     .
     .
     .");
-}
 
-fn print_instructions() {
-    println!("
-    Please input a list of numbers, delineateed by a comma:");
+    let mut numbers_list: Vec<u32> = Vec::new();
+
+    loop {
+        println!("Please input a number to the list, or a non-number to end inputs:");
+
+        let mut current_input = String::new();
+
+        io::stdin().read_line(&mut current_input).expect("Failed to read line");
+
+        let numerical_input: u32 = match current_input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => break
+        };
+
+        numbers_list.push(numerical_input);
+    }
+
+    println!("Input done.");
+    println!("{:?}", numbers_list);
+
 }
