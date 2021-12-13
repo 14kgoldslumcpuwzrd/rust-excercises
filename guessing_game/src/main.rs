299b0d62b0c8b39d59b86ae07d3662f2fffa2f13
ashaunthomas <ashaunthomas@siu.edu>
2021-12-12 23:43:57 -0800

    adds final touches to guessing-game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -10,14 +10,13 @@ fn main() {
     
     
     loop {
-        println!("The secret number is: {}", secret_number);
         println!("Please input your guess:");
 
         let mut guess = String::new();
     
         io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse() {
+        let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
